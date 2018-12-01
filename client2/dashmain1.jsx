a5700b085c3771eff7785675b287ddd5fb7d8ed4
Paco Sánchez M <pacosanchezm@MacBook-Pro-3.local>
2018-12-01 15:27:13 -0600

    main1

--- client2/dashmain1.jsx
+++ client2/dashmain1.jsx
@@ -1,1 +1,40 @@
 // ------- Dashmain
+
+
+
+
+
+
+export default class Main extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      page: "777",
+
+
+
+    };
+  } // ------------------------- Constructor
+
+
+
+
+
+    componentWillMount() {
+      this.getdatoschart1();
+    }
+
+
+
+
+
+  render() {
+    return <div>
+
+    hola de nuevo
+
+
+    </div>;
+  }
+}
