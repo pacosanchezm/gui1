2ac39946fc4cced21655e70a9c3e225bf404c85c
pacosanchezm <paco.sanchezm@gmail.com>
2019-01-07 09:12:52 -0600

    cleanin

--- client2/livemensajes.jsx
+++ client2/livemensajes.jsx
@@ -16,7 +16,7 @@ import moment from "moment";
 
 // ------------------------------------------------------------
 
-let Micolor = true;
+let Micolor = true
 
 const Encabezado = props => {
   try {
@@ -27,73 +27,69 @@ const Encabezado = props => {
         <div>
           <ThemeProvider theme={props.Theme}>
             <div>
-              <cssfibo.MyFlex1
-                css={{ backgroundColor: props.Theme.backgroundcolor }}
-              >
+              <cssfibo.MyFlex1 css={{ backgroundColor: props.Theme.backgroundcolor }}>
+
                 <cssfibo.Box1
-                  css={{
+                css={{
                     width: 377,
                     backgroundColor: props.Theme.backgroundcolor,
                     height: 34
                   }}
-                >
+              >
                   <cssfibo.h1
-                    text="Mensajes"
-                    size="18"
-                    color="White"
-                    weight="Bold"
-                    style="Normal"
-                  />
+                  text="Mensajes"
+                  size="18"
+                  color="White"
+                  weight="Bold"
+                  style="Normal"
+                />
                 </cssfibo.Box1>
+
               </cssfibo.MyFlex1>
 
-              <cssfibo.MyFlex1
-                css={{ backgroundColor: props.Theme.backgroundcolor }}
-              >
-                <cssx.box3label css={{ width: "123px" }}>
+
+              <cssfibo.MyFlex1 css={{ backgroundColor: props.Theme.backgroundcolor }}>
+
+                <cssx.box3label css={{ width: "89px" }}>
                   <cssx.h3>Nombre</cssx.h3>
                 </cssx.box3label>
 
-                <cssx.box3label css={{ width: "121px" }}>
+                <cssx.box3label css={{ width: "123px" }}>
                   <cssx.h3>Origen</cssx.h3>
                 </cssx.box3label>
 
-                <cssx.box3label css={{ width: "144px" }}>
+                <cssx.box3label css={{ width: "233px" }}>
                   <cssx.h3>Mensaje</cssx.h3>
                 </cssx.box3label>
+
               </cssfibo.MyFlex1>
             </div>
           </ThemeProvider>
         </div>
-      );
-    };
+      )
+    }
 
-    return children({
-      Seccion1: Seccion1()
-    });
-  } catch (e) {
-    console.error(e);
-  }
-};
+    return children({Seccion1: Seccion1()})
+
+  } catch (e) {console.error(e)}
+}
 
 const Listado1 = props => {
   try {
-    const { children } = props;
-
     const Renglon1 = props => {
       let BgColor;
-      if (props.RenglonColor === false) {
-        BgColor = "White";
-      }
-      if (props.RenglonColor === true) {
-        BgColor = "WhiteSmoke";
-      }
+      if (props.RenglonColor === false) {BgColor = "White"}
+      if (props.RenglonColor === true) {BgColor = "WhiteSmoke"}
+
+      if (props.Row.Status === "Live") { BgColor = "#ffd6d6" }
+      if (props.Row.Status === "Listo") { BgColor = "#c6ecd6" }
 
       return (
         <ThemeProvider theme={props.Theme}>
           <div>
             <cssfibo.MyFlex1 css={{ backgroundColor: BgColor }}>
-              <cssx.box3label css={{ width: "110px" }}>
+
+              <cssx.box3label css={{ width: "89px" }}>
                 <cssx.h3>{props.Row.Nombre}</cssx.h3>
               </cssx.box3label>
 
@@ -101,38 +97,74 @@ const Listado1 = props => {
                 <cssx.h3>{props.Row.Origen}</cssx.h3>
               </cssx.box3label>
 
-              <cssx.box3label css={{ width: "144px", paddingRight: 21 }}>
+              <cssx.box3label css={{ width: "233px", paddingRight: 21 }}>
                 <cssx.h3>{props.Row.Mensaje}</cssx.h3>
               </cssx.box3label>
+
+
+
+              <cssfibo.Boton1
+              class="noatiende"
+              color={"Red"}
+              onClick={() => {
+                props.this.StatusU(props.Row.Id, 'Live')
+               }}
+             >
+                Live
+              </cssfibo.Boton1>
+
+
+              <cssfibo.Boton1
+              class="noatiende"
+              color={"SeaGreen"}
+              onClick={() => {props.this.StatusU(props.Row.Id, 'Listo')}}
+             >
+                Listo
+              </cssfibo.Boton1>
+
+
+              <cssfibo.Boton1
+              class="noatiende"
+              color={"SlateGrey"}
+              onClick={() => {
+                props.this.StatusU(props.Row.Id, 'Oculto')
+               }}
+             >
+                Ocultar
+              </cssfibo.Boton1>
+
+
             </cssfibo.MyFlex1>
           </div>
         </ThemeProvider>
-      );
-    };
+      )
+    }
 
     let MiMapa = props.Registros.map(row => {
       return (
         <div>
-          <Renglon1 RenglonColor={Micolor} Row={row} Theme={theme3.renglon} />
-
-          {(() => {
-            Micolor = !Micolor;
-          })()}
+          <Renglon1
+            key={row.Id}
+            RenglonColor={Micolor}
+            Row={row}
+            Theme={theme3.renglon}
+            this={props.this}
+          />
+          {(() => {Micolor = !Micolor})()}
         </div>
-      );
-    });
+      )
+    })
 
-    return <div>{MiMapa}</div>;
-  } catch (e) {
-    console.error(e);
-  }
-};
+    return <div>{MiMapa}</div>
+
+  } catch (e) {console.error(e)}
+}
 
 const LoadingSpinner = () => (
   <div>
     <cssx.h3> Cargando...</cssx.h3>
   </div>
-);
+)
 
 //--------------------------------------------------------------
 
@@ -140,13 +172,10 @@ const LoadingSpinner = () => (
 
 export default class Lista extends React.PureComponent {
   constructor(props) {
-    super(props);
+    super(props)
 
     this.state = {
       orden: "Id",
-
-      botonguardar: "Grey",
-      status: [{ value: "", label: "" }],
 
       loading: false,
 
@@ -161,48 +190,72 @@ export default class Lista extends React.PureComponent {
           Edad: ""
         }
       ]
-    };
+    }
   } // ------------------------- Constructor
 
   componentWillMount() {
-    this.getdatos();
+    this.getdatos()
   }
 
   getdatos = async () => {
     try {
-      this.setState({ loading: true });
-
+    //  this.setState({ loading: true });
       var axdata = await axios({
         url: "https://smxai.net/graphqlpub",
         method: "post",
         data: {
           query: `
-                query LiveMensajes($Query: LiveMensajeInput) {
-                  LiveMensajes(Query: $Query) {
-                    Id, Status, Mensaje, Nombre, Origen, Genero, Edad
-                  }
-                }
-              `,
+            query LiveMensajes($Query: LiveMensajeInput) {
+              LiveMensajes(Query: $Query) {
+                Id, Status, Mensaje, Nombre, Origen, Genero, Edad
+              }
+            }
+          `,
           variables: {
             Query: {
-              Status: "Activo"
+         //     Status: "Live"
             }
           }
         }
-      });
+      })
 
-      let data = axdata.data.data.LiveMensajes;
+      let data = axdata.data.data.LiveMensajes
+      this.setState({ Registros: data })
+      this.setState({ loading: false })
 
-      this.setState({ Registros: data });
-      this.setState({ loading: false });
-    } catch (e) {
-      console.error(e);
-    }
-  };
+    } catch (e) {console.error(e)}
+  }
+
+
+  StatusU = async (Id, Status) => {
+    try {
+      await axios({
+        url: "https://smxai.net/graphqlpub",
+        method: "post",
+        data: {
+          query: `
+            mutation LiveMensajeU($Reg: LiveMensajeInput) {
+              LiveMensajeU(Reg: $Reg)
+            }
+          `,
+          variables: {
+            Reg: {
+              Id: Id,
+              Status: Status
+            }
+          }
+        }
+      })
+
+      this.getdatos()
+
+    } catch (e) {console.error(e)}
+  }
+
 
   cerrar = () => {
-    WebviewControls.close();
-  };
+    WebviewControls.close()
+  }
 
   // Render ------------------------------------------------------------------------
 
@@ -213,27 +266,25 @@ export default class Lista extends React.PureComponent {
       <div>
         <cssfibo.MyFlex3 css={{ gridArea: "header" }}>
           <Encabezado Theme={theme3.encabezado} this={this}>
-            {({ Seccion1 }) => (
-              <div>
-                <div>{Seccion1}</div>
-              </div>
-            )}
+            {({Seccion1}) => (<div>{Seccion1}</div>)}
           </Encabezado>
         </cssfibo.MyFlex3>
 
         <cssfibo.MyFlex3 css={{ gridArea: "contenido" }}>
           <ThemeProvider theme={theme3.forma}>
             <div>
-              {loading ? (
-                <LoadingSpinner />
-              ) : (
-                <Listado1 Theme={theme3.encabezado} Registros={Registros} />
-              )}
+              {loading ? (<LoadingSpinner />) : 
+              (<Listado1
+                Key={1}
+                Theme={theme3.encabezado}
+                Registros={Registros}
+                this={this}
+              />)}
             </div>
           </ThemeProvider>
         </cssfibo.MyFlex3>
       </div>
-    );
+    )
   }
 
   // ---------------------------------------------------------------- Render
